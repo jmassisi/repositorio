675da6cfdf13a2de218a02b72ebacd8cb40dfabf
Juan Manuel Assisi <jmassisi@gmail.com>
2026-04-26 17:57:10 -0300

    Abrir carpeta al finalizar

--- repositorio.ps1
+++ repositorio.ps1
@@ -4,3 +4,6 @@ Expand-Archive $z $env:TEMP\rextract -Force
 Move-Item "$env:TEMP\rextract\repositorio-main" $d -Force
 ri $z,$env:TEMP\rextract -Recurse -Force
 Remove-Item (Get-PSReadLineOption).HistorySavePath -EA 0;Clear-History
+Write-Host "Listo. Presione cualquier tecla para cerrar y abrir la carpeta..." -ForegroundColor Green
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+Start-Process explorer.exe $d
