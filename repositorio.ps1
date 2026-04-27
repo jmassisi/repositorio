2c0756b8bf46d0e91a3622d15052299adbc0345e
Juan Manuel Assisi <jmassisi@gmail.com>
2026-04-27 18:35:09 -0300

    Fix: sobreescribir repositorio existente al actualizar

--- repositorio.ps1
+++ repositorio.ps1
@@ -1,11 +1,11 @@
 $d="C:\repositorio";$z="$env:TEMP\r.zip"
 irm https://github.com/jmassisi/repositorio/archive/refs/heads/main.zip -OutFile $z
 Expand-Archive $z $env:TEMP\rextract -Force
-Move-Item "$env:TEMP\rextract\repositorio-main" $d -Force
-ri $z,$env:TEMP\rextract -Recurse -Force
+Remove-Item $d\* -Recurse -Force -Exclude "repositorio-main"
+Move-Item "$env:TEMP\rextract\repositorio-main\*" $d -Force
+Remove-Item "$env:TEMP\rextract" -Recurse -Force
+Remove-Item $z -Force
 Remove-Item (Get-PSReadLineOption).HistorySavePath -EA 0;Clear-History
 Write-Host "Listo. Presione cualquier tecla para cerrar y abrir la carpeta..." -ForegroundColor Green
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 Start-Process explorer.exe $d
-
-exit
