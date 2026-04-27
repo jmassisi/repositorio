456e25edaa31d6f2a12b0877dab8f3e500f2ed64
Juan Manuel Assisi <jmassisi@gmail.com>
2026-04-27 18:39:49 -0300

    Fix: crear o sobreescribir repositorio correctamente

--- repositorio.ps1
+++ repositorio.ps1
@@ -1,8 +1,8 @@
 $d="C:\repositorio";$z="$env:TEMP\r.zip"
 irm https://github.com/jmassisi/repositorio/archive/refs/heads/main.zip -OutFile $z
 Expand-Archive $z $env:TEMP\rextract -Force
-Remove-Item $d\* -Recurse -Force -Exclude "repositorio-main"
-Move-Item "$env:TEMP\rextract\repositorio-main\*" $d -Force
+if (Test-Path $d) { Remove-Item $d -Recurse -Force }
+Move-Item "$env:TEMP\rextract\repositorio-main" $d -Force
 Remove-Item "$env:TEMP\rextract" -Recurse -Force
 Remove-Item $z -Force
 Remove-Item (Get-PSReadLineOption).HistorySavePath -EA 0;Clear-History
